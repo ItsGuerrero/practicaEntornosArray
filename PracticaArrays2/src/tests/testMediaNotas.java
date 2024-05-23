de6283f2a71f05735decccf38e80381ddbdd87bc
ItsGuerrero <jguerrerobelmonte@gmail.com>
2024-05-23 10:06:17 +0200

    añadido test Maxima nota

--- PracticaArrays2/src/tests/testMediaNotas.java
+++ PracticaArrays2/src/tests/testMediaNotas.java
@@ -2,13 +2,34 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import util.misArrays;
 
 class testMediaNotas {
 
+	static int[] notasValidas;
+	static int[] notasNoValidas;
+    
+	@BeforeAll
+	public static void inicializarDatos() {
+        notasValidas = new int[]{1, 2, 3, 4, 5};
+        notasNoValidas = new int[]{-1, 13, -8, -7, 25};
+    }
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testMediaNotasValoresValidos() {
+		
+        float mediaCalculada = misArrays.mediaNotas(notasValidas);
+
+        assertEquals(3, mediaCalculada);
+    }
+	
+	@Test
+	void testMediaNotaValoresNoValidos() {
+		
+        assertThrows(IllegalArgumentException.class, () -> misArrays.mediaNotas(notasNoValidas));
+    }
 
 }
